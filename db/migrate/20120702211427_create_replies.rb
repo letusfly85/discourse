867452296d98282aece80a908f68a74e3151e019
shunsuke wada <letusfly95@gmail.com>
2015-03-25 00:31:26 +0900

    it is wrong to use Capitalized alias 'P'

--- db/migrate/20120702211427_create_replies.rb
+++ db/migrate/20120702211427_create_replies.rb
@@ -11,7 +11,7 @@ class CreateReplies < ActiveRecord::Migration
     execute "INSERT INTO post_replies (post_id, reply_id, created_at, updated_at)
              SELECT p2.id, p.id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
              FROM posts AS p
-             INNER JOIN posts AS p2 on p2.post_number = p.reply_to_post_number AND p2.forum_thread_id = P.forum_thread_id
+             INNER JOIN posts AS p2 on p2.post_number = p.reply_to_post_number AND p2.forum_thread_id = p.forum_thread_id
              WHERE p.forum_thread_id IS NOT NULL"
   end
 end
